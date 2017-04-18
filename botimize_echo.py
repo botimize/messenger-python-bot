dd53e2f81b7d701609970ada4c0cf42141cc488c
chhsu <chhsu@botimize.io>
2017-04-18 15:05:17 +0800

    Modified return code

--- botimize_echo.py
+++ botimize_echo.py
@@ -19,14 +19,7 @@ def reply(user_id, msg):
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
-
-    return "Hello world", 200
+    return request.args['hub.challenge']
  
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
