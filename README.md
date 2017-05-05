cecfa635f4bc0ab61569037d94428944af03f380
chhsu <chhsu@botimize.io>
2017-05-05 14:06:03 +0800

    Hide api key

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup
+```
+pip install -r requirements.txt
+```
+# Host on heroku
+This project use heroku as our host.
+You may create a project on heroku and copy the heroku webhook to the facebook developer.
+
--- botimize_echo.py
+++ botimize_echo.py
@@ -6,16 +6,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
  
-ACCESS_TOKEN = 'EAAYZCSs2uV0sBAFvOcMBGPOtSzTnEJQ6Jpp36UFZC40qcH0noQSiWubjTzkOvyfU1uOQfH7ZCYb5mrZApv72WEWDjBCBpwyn7bKnMNhKGByZCzZAnLZCQMk9ZCbsOrWnLkddKc9lLCdSOC2OlYzFBiZAoSAMkNMM8XZC1OQXdt9yFcTAZDZD' 
-botimize = Botimize('7WAYASBD1MA9LRSUMJ5EZWA7K40P6RRY', 'facebook')
+FACEBOOK_ACCESS_TOKEN = 'Your_Facebook_Access_Token'
+Botimize_Api_Key = 'Your_Botimize_Api_Key'
+botimize = Botimize(Botimize_Api_Key, 'facebook')
  
 def reply(user_id, msg):
     data = {
         "recipient": {"id": user_id},
         "message": {"text": msg}
     }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    print(resp.content)
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + FACEBOOK_ACCESS_TOKEN, json=data)
+    return "ok"
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -23,15 +24,16 @@ def verify():
  
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
+    # reply
+    data_in = request.json
+    sender = data_in['entry'][0]['messaging'][0]['sender']['id']
+    message = data_in['entry'][0]['messaging'][0]['message']['text']
+    reply(sender, message)
 
     # incoming
-    data = request.json
-    botimize.log_incoming(data)
+    botimize.log_incoming(data_in)
 
     #outgoing
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    reply(sender, message)
     data_out = {
         "recipient": {"id": sender},
         "message": {"text": message}
